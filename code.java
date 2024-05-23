7f5c6023a8164befeaa67c164904e21fc9294a32
nadir-n <Nadir.n0957@gmail.com>
2024-05-23 19:56:53 +0500

    "adds lucky colour function"

--- code.java
+++ code.java
@@ -16,35 +16,81 @@ class code {
         int LPN = LifePathNumber(date, mon, year);
         System.out.println("Life Path Number: " + LPN);
 
+        String Lucky_Colour = LuckyColour(LPN);
+        System.out.println("Lucky Colour: " + Lucky_Colour);
+
+    }
+
+    public static String LuckyColour(int lpn) {
+        String colour = "";
+        if (lpn == 1) {
+            colour = "Red";
+        } 
+        else if (lpn == 2) {
+            colour = "Orange";
+        } 
+        else if (lpn == 3) {
+            colour = "Yellow";
+        } 
+        else if (lpn==4){
+            colour = "Green";
+        }
+        else if (lpn == 5) {
+            colour = "Sky Blue";
+        }
+        else if (lpn==6){
+            colour="Indigo";
+        }
+        else if(lpn ==7){
+            colour="Violet";
+        }
+        else if(lpn==8){
+            colour="Magenta";
+        }
+        else if(lpn==9){
+            colour="Gold";
+        }
+        else if(lpn==11){
+            colour="Silver";
+        }
+        else if(lpn==22){
+            colour="White";
+        }
+        else if(lpn==33){
+            colour="Crimson";
+        }
+        return colour;
+
     }
 
     public static int LifePathNumber(int date, int month, int year) {
         int date_lpn = sumLPN(date);
 
         int month_lpn = sumLPN(month);
-        
+
         int year_lpn = sumLPN(year);
 
         int final_lpn = sumLPN(date_lpn + month_lpn + year_lpn);
         return (final_lpn);
     }
-    public static int sumLPN(int num){
+
+    public static int sumLPN(int num) {
         int lpn = 0;
         int temp = num;
-        
-        do{
-            lpn=0;
-            if(temp==11 || temp==22 || temp==33){
-                lpn=temp;
+
+        do {
+            lpn = 0;
+            if (temp == 11 || temp == 22 || temp == 33) {
+                lpn = temp;
                 break;
             }
             while (temp > 0) {
                 lpn += temp % 10;
                 temp /= 10;
             }
-            temp=lpn;
-            
-        }while (temp / 10 > 0);
+            temp = lpn;
+
+        } while (temp / 10 > 0);
         return lpn;
     }
 
